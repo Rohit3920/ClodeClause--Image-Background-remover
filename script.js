a78d34d8568f8c3d886b5f11a683e2cfdd668659
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-08-01 15:57:19 +0530

    JavaScript : Function uploadImage are created to fetching a data for API to show image in the console

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 console.warn("This is a CodeClause Internship project no1 ");
 console.log("Image Background Remover");
+
+
+function uploadImage() {
+    //    alert("Uplaod btn clicked")
+
+    const fileInput = document.getElementById('chooseFile');
+    const image = fileInput.files[0];
+
+    const formData = new FormData();
+    formData.append("image_file", image);
+    formData.append('size', 'auto');
+
+    let url = 'https://api.remove.bg/v1.0/removebg';
+    const apiKey = 'ixMeo8XoRiSgapyLJ7jgPQo9'
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'x-api-key': apiKey,
+        },
+        body: formData,
+    }).then((res) => {
+        return res.blob();
+    }).then((data) => {
+        console.log(data)
+    }).catch((err) => {
+        console.log(err + "this error was catch")
+    })
+}
